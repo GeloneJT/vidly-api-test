ef549f270ae53521ca1779f22239a1a175909d55
Jelani <eyeglass0324@gmail.com>
2022-04-18 09:56:09 -0400

    routes moved to seperate file

--- index.js
+++ index.js
@@ -5,19 +5,11 @@
  */
 const { default: mongoose } = require('mongoose');
 const express = require('express');
-const morgan = require('morgan');
 const config = require('config');
 const winston = require('winston');
-require('winston-mongodb');
-const genres = require('./routes/genres');
-const home = require('./routes/home');
-const customers = require('./routes/customers');
-const movies = require('./routes/movies');
-const rentals = require('./routes/rentals');
-const users = require('./routes/users');
-const auth = require('./routes/auth');
-const error = require('./middleware/error');
 const app = express();
+require('winston-mongodb');
+require('./startup/routes')(app)
 
 winston.createLogger({
   format: winston.format.metadata(),
@@ -43,9 +35,9 @@ winston.rejections.handle(
   })
 );
 
-// throw new Error('Something failed durting startup'); //  Uncomment to simulate failed server startup
-const p = Promise.reject(new Error('Failed miserably'))
-p.then(() => console.log('Done'));
+// throw new Error('Something failed durting startup'); //  Create an unhandled  exception on server startup
+// const p = Promise.reject(new Error('Failed miserably'))  // Create an unhandled rejection
+// p.then(() => console.log('Done'));
 
 if (!config.get('jwtPrivateKey')) {
   console.error('FATAL ERROR: jwtPrivateKey is not defined');
@@ -57,17 +49,7 @@ mongoose
   .then(() => console.log('Connected to MongoDB....'))
   .catch((err) => console.error('Could not connect to MongoDB...', err));
 
-app.use(express.json());
-app.use(morgan('dev'));
-app.use('/api/genres', genres);
-app.use('/api/customers', customers);
-app.use('/api/movies', movies);
-app.use('/api/rentals', rentals);
-app.use('/api/users', users);
-app.use('/api/auth', auth);
-app.use('/', home);
 
-app.use(error);
 
 //  Creates a server
 const port = process.env.PORT || 3000;
