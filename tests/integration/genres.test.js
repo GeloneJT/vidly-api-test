e6e06c7f55264cb97259d3f55b8cb122bcbff8db
Jelani <eyeglass0324@gmail.com>
2022-04-20 16:44:57 -0400

    genres test populates to simulate data, tests the routes, and the cleans up after itself

--- tests/integration/genres.test.js
+++ tests/integration/genres.test.js
@@ -1,18 +1,27 @@
 const request = require('supertest');
+const { Genre } = require('../../models/genre');
 let server;
 
 describe('/api/genres', () => {
   beforeEach(() => {
     server = require('../../index');
   });
-  afterEach(() => {
+  afterEach(async () => {
     server.close();
+    await Genre.remove({});
   });
 
   describe('GET /', () => {
     it('should return all genres', async () => {
+      await Genre.collection.insertMany([
+        { type: 'genre1' },
+        { type: 'genre2' },
+      ]);
       const res = await request(server).get('/api/genres');
       expect(res.status).toBe(200);
+      expect(res.body.length).toBe(2);
+      expect(res.body.some(g => g.type === 'genre1')).toBeTruthy();
+      expect(res.body.some(g => g.type === 'genre2')).toBeTruthy();
     });
   });
 });
