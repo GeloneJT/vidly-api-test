84b1bd5554df57a52ad2573dcc894ed4b45d58ae
Jelani <eyeglass0324@gmail.com>
2022-04-22 15:46:29 -0400

    validate middleware added. rental static and instance method added. static and instance method added to returns route.

--- routes/returns.js
+++ routes/returns.js
@@ -1,28 +1,29 @@
+const Joi = require('joi');
 const moment = require('moment');
 const express = require('express');
 const router = express.Router();
+const validate = require('../middleware/validate');
 const auth = require('../middleware/auth');
 const { Rental } = require('../models/rental');
 const { Movie } = require('../models/movie');
 
-router.post('/', auth, async (req, res) => {
+router.post('/', [auth], async (req, res) => {
+  FIXME: 'connect this to work with the validateReturn function';
+  // const { error } = validateReturn(req.body);
+  // if (error) return res.status(400).send(error.details[0].message);
+
   if (!req.body.customerId)
     return res.status(400).send('customerId not provided');
   if (!req.body.movieId) return res.status(400).send('movieId not provided');
 
-  const rental = await Rental.findOne({
-    'customer._id': req.body.customerId,
-    'movie._id': req.body.movieId,
-  });
+  const rental = await Rental.lookup(req.body.customerId, req.body.movieId);
 
   if (!rental) return res.status(404).send('No rental found');
   if (rental.dateReturned)
     return res.status(400).send('Return already processed');
 
-  rental.dateReturned = new Date();
-  const rentalDays = moment().diff(rental.dateOut, 'days');
-  rental.rentalCost = rentalDays * rental.movie.dailyRentalRate;
-  
+  rental.return();
+
   await rental.save();
 
   await Movie.updateOne(
@@ -34,5 +35,14 @@ router.post('/', auth, async (req, res) => {
 
   return res.status(200).send(rental);
 });
+
+FIXME: ' validateReturn currently breaks the route';
+function validateReturn(req) {
+  const schema = Joi.object({
+    customerId: Joi.objectId().required(),
+    movieId: Joi.objectId().required(),
+  });
+  return schema.validate(req, Joi);
+}
 
 module.exports = router;
