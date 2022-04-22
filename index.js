e5e8614eb189e24cd3a5862a7f53842413b3d96c
Jelani <eyeglass0324@gmail.com>
2022-04-22 16:23:51 -0400

    setup for heorku deployment

--- index.js
+++ index.js
@@ -3,16 +3,19 @@
  * The following program is built to simulate a movie genre directory webpage
  *  It showcases the simplicities of using Express.js and CRUD methods
  */
-const winston = require('winston')
+const winston = require('winston');
 const express = require('express');
 const app = express();
 require('./startup/logging')();
 require('./startup/routes')(app);
 require('./startup/db_connect')();
 require('./startup/config')();
+require('./startup/prod')(app);
 
 //  Creates a server
 const port = process.env.PORT || 3000;
-const server = app.listen(port, () =>winston.info(`Listening on port ${port}....`));
- 
+const server = app.listen(port, () =>
+  winston.info(`Listening on port ${port}....`)
+);
+
 module.exports = server;
